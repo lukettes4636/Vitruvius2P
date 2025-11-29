29f4d69ad8ed2b012176423545afbf1d3df55d83
Lalucho42 <francolucianobarbona@gmail.com>
2025-11-29 06:16:45 -0300

    Add noise ring VFX and integrate with player noise
    Introduced a new VFX_NoiseRing visual effect and integrated it into the PlayerNoiseEmitter system. Updated the DaVinciPB scene to include the new NoiseParticles GameObject and linked the VFX to the player script for visualizing noise radius. Also made minor adjustments to ButtonIndicator prefab and shockwave VFX gradient.

--- Assets/scripts/Players/PlayerNoiseEmitter.cs
+++ Assets/scripts/Players/PlayerNoiseEmitter.cs
@@ -1,8 +1,5 @@
 ﻿using UnityEngine;
-
-
-
-
+using UnityEngine.VFX; 
 
 [RequireComponent(typeof(CharacterController))]
 public class PlayerNoiseEmitter : MonoBehaviour
@@ -13,69 +10,100 @@ public class PlayerNoiseEmitter : MonoBehaviour
     public float crouchNoiseRadius = 2f;
     public float runNoiseRadius = 6f;
 
-    [Header("Visual Debug")]
+    [Header("Visual Feedback")]
+    public VisualEffect noiseVFX; 
+    public string vfxRadiusProperty = "Radius"; 
+    public float visualLerpSpeed = 5f; 
+
+    [Header("Debug")]
     public bool showNoiseGizmo = true;
     public Color noiseColor = new Color(1f, 0.6f, 0f, 0.25f);
 
     [HideInInspector] public float currentNoiseRadius = 0f;
+
     private CharacterController controller;
+    private float visualRadius = 0f; 
 
     
-    private MovJugador1 mov1;
-    private MovJugador2 mov2;
+    private object activeMovementScript;
+    private System.Reflection.FieldInfo isMovingField, isRunningField, isCrouchingField;
+    private bool initialized = false;
 
     void Awake()
     {
         controller = GetComponent<CharacterController>();
-        mov1 = GetComponent<MovJugador1>();
-        mov2 = GetComponent<MovJugador2>();
+        InitializeReflection();
+    }
+
+    void InitializeReflection()
+    {
+        var m1 = GetComponent("MovJugador1");
+        var m2 = GetComponent("MovJugador2");
+        if (m1 != null) activeMovementScript = m1;
+        else if (m2 != null) activeMovementScript = m2;
+
+        if (activeMovementScript != null)
+        {
+            var t = activeMovementScript.GetType();
+            isMovingField = t.GetField("isMoving", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+            isRunningField = t.GetField("isRunningInput", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+            isCrouchingField = t.GetField("isCrouching", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+            initialized = true;
+        }
     }
 
     void Update()
     {
-        UpdateNoiseRadius();
+        CalculateLogicRadius();
+        UpdateVFX();
     }
 
-    private void UpdateNoiseRadius()
+    void CalculateLogicRadius()
     {
-        bool isMoving = false;
-        bool isRunning = false;
-        bool isCrouching = false;
+        bool isMoving = false, isRunning = false, isCrouching = false;
 
-        if (mov1 != null)
+        if (initialized)
         {
-            
-            isMoving = (bool)mov1.GetType()
-                .GetField("isMoving", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)
-                ?.GetValue(mov1);
-            isRunning = (bool)mov1.GetType()
-                .GetField("isRunningInput", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)
-                ?.GetValue(mov1);
-            isCrouching = (bool)mov1.GetType()
-                .GetField("isCrouching", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)
-                ?.GetValue(mov1);
+            if (isMovingField != null) isMoving = (bool)isMovingField.GetValue(activeMovementScript);
+            if (isRunningField != null) isRunning = (bool)isRunningField.GetValue(activeMovementScript);
+            if (isCrouchingField != null) isCrouching = (bool)isCrouchingField.GetValue(activeMovementScript);
         }
-        else if (mov2 != null)
+        else
         {
-            
-            isMoving = GetComponent<CharacterController>().velocity.magnitude > 0.1f;
-            isRunning = (bool)mov2.GetType()
-                .GetField("isRunningInput", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)
-                ?.GetValue(mov2);
-            isCrouching = (bool)mov2.GetType()
-                .GetField("isCrouching", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance)
-                ?.GetValue(mov2);
+            isMoving = controller.velocity.magnitude > 0.1f;
         }
+
+        float target = idleNoiseRadius;
+        if (isMoving)
+        {
+            if (isRunning) target = runNoiseRadius;
+            else if (isCrouching) target = crouchNoiseRadius;
+            else target = walkNoiseRadius;
+        }
+
+        currentNoiseRadius = target;
+    }
+
+    void UpdateVFX()
+    {
+        if (noiseVFX == null) return;
 
         
-        if (!isMoving)
-            currentNoiseRadius = idleNoiseRadius;
-        else if (isCrouching)
-            currentNoiseRadius = crouchNoiseRadius;
-        else if (isRunning)
-            currentNoiseRadius = runNoiseRadius;
+        visualRadius = Mathf.Lerp(visualRadius, currentNoiseRadius, Time.deltaTime * visualLerpSpeed);
+
+        
+        noiseVFX.SetFloat(vfxRadiusProperty, visualRadius);
+
+        
+        
+        if (currentNoiseRadius <= idleNoiseRadius)
+        {
+            noiseVFX.SetFloat("SpawnRate", 10); 
+        }
         else
-            currentNoiseRadius = walkNoiseRadius;
+        {
+            noiseVFX.SetFloat("SpawnRate", 100);
+        }
     }
 
     void OnDrawGizmosSelected()
